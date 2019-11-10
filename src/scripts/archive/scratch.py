f3bd51a0ea827df1c8b4527076e9989e720f0f7d
matentzn <nicolas.matentzoglu@gmail.com>
2019-11-10 22:18:21 +0000

    Update scratch.py

--- src/scripts/archive/scratch.py
+++ src/scripts/archive/scratch.py
@@ -3,12 +3,12 @@ import os
 import yaml
 import pandas as pd
 
-idmf=os.path.join("~/ws/zebrafish-phenotype-ontology/src/curation","id_map_zfin.tsv")
+idmf=os.path.join("~/ws/zebrafish-phenotype-ontology/src/patterns/data/anatomy","abnormalAnatomicalEntity.tsv")
 remf=os.path.join("~/ws/zebrafish-phenotype-ontology/src/ontology","id_map_rem.txt")
 rem = pd.read_csv(remf, header=None, sep='\t')
 idm = pd.read_csv(idmf, sep='\t')
 print(idm.head())
 remids = list(rem.ix[:,0].values)
 print(remids)
-idm = idm[~idm['iri'].isin(remids)]
+idm = idm[~idm['defined_class'].isin(remids)]
 idm.to_csv(idmf, sep='\t', index=False)
