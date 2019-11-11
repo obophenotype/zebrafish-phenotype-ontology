c12f8571ce8428cafd3e708d97657dc71ca2e826
matentzn <nicolas.matentzoglu@gmail.com>
2019-11-11 16:14:35 +0000

    Update scratch.py

--- src/scripts/archive/scratch.py
+++ src/scripts/archive/scratch.py
@@ -3,12 +3,24 @@ import os
 import yaml
 import pandas as pd
 
-idmf=os.path.join("~/ws/zebrafish-phenotype-ontology/src/patterns/data/anatomy","abnormalAnatomicalEntity.tsv")
-remf=os.path.join("~/ws/zebrafish-phenotype-ontology/src/ontology","id_map_rem.txt")
-rem = pd.read_csv(remf, header=None, sep='\t')
+idmf="/Users/matentzn/ws/zebrafish-phenotype-ontology/src/curation/zp_zfin_phenotype_fish.tsv"
+redf="/Users/matentzn/ws/zebrafish-phenotype-ontology/src/curation/zp_zfin_phenotype_fish_go_quality.tsv"
+remf="/Users/matentzn/ws/zebrafish-phenotype-ontology/src/curation/fix_equivalents.txt"
+fe = pd.read_csv(remf, sep='\t')
 idm = pd.read_csv(idmf, sep='\t')
-print(idm.head())
-remids = list(rem.ix[:,0].values)
-print(remids)
-idm = idm[~idm['defined_class'].isin(remids)]
-idm.to_csv(idmf, sep='\t', index=False)
+
+#rel=list(fe['rel'].values)
+#print(rel)
+#anatomy=list(fe['anatomy'].values)
+#partof=list(fe['partof'].values)
+
+#idm = idm[idm['Post-composed Relationship ID'].isin([''])]
+print(len(idm))
+idm = idm.loc[idm['Affected Structure or Process 1 subterm ID'].str.startswith('GO:', na=False)]
+print(len(idm))
+idm = idm.loc[idm['Phenotype Keyword ID']=='PATO:0000001']
+print(len(idm))
+#print(len(idm))
+#idm = idm[idm['Affected Structure or Process 1 superterm ID'].isin(partof)]
+#print(len(idm))
+idm.to_csv(redf, sep='\t', index=False)
