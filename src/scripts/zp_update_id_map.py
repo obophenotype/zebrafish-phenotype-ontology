dc35abdfaafd959bdce5a3d1aea9cc62c8714923
matentzn <nicolas.matentzoglu@gmail.com>
2019-11-11 14:32:13 +0000

    new release

--- src/scripts/zp_update_id_map.py
+++ src/scripts/zp_update_id_map.py
@@ -96,8 +96,9 @@ d['id'] = d['id_raw'].apply(lambda x: correct_id(x))
 # Merge the fresh set of annotations with the current set of ZP identifiers
 zfin_id_map_merged = pd.merge(d, id_map, on='id', how='left')
 zfin_id_map_merged = zfin_id_map_merged.drop_duplicates()
+check_broken = zfin_id_map_merged[['iri','id']].drop_duplicates()
 # Make sure there are no duplicate ids.
-broken = get_rows_with_duplicates(zfin_id_map_merged,'iri')
+broken = get_rows_with_duplicates(check_broken,'iri')
 if len(broken)>0:
     e = str(broken[['id_raw','iri']])
     #broken.to_csv("broken_idmap.tsv", sep = '\t', index=False)
