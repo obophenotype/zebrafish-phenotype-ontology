0af7d908e8f0788b86458c67fc52bb32f98e3574
matentzn <nicolas.matentzoglu@gmail.com>
2020-01-21 14:18:06 +0000

    Update zp_release.sh

--- src/ontology/zp_release.sh
+++ src/ontology/zp_release.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
+IMP=true
+PAT=true
 sh run.sh make IMP=false zp_pipeline -B
-sh run.sh make IMP=false PAT=false SRC=zp-edit-release.owl prepare_release -B
+sh run.sh make IMP=$IMP PAT=$PAT SRC=zp-edit-release.owl prepare_release -B
