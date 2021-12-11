5ecaa7340505295f44c3af0b4347b318de5961a6
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2021-12-11 20:27:42 +0200

    Update

--- src/ontology/zp_release.sh
+++ src/ontology/zp_release.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
-IMP=true
+IMP=false
 PAT=true
 sh run.sh make IMP=false PAT=false zp_pipeline_prepare_data -B
 sh run.sh make IMP=false PAT=true templates patterns -B
-sh run.sh make IMP=true PAT=false all_imports -B
+sh run.sh make IMP=$IMP PAT=false all_imports -B
 sh run.sh make IMP=false PAT=true templates patterns -B
-sh run.sh make IMP=true PAT=true preprocess -B
+sh run.sh make IMP=$IMP PAT=true preprocess -B
 sh run.sh make IMP=false PAT=false SRC=zp-edit-release.owl prepare_release -B
