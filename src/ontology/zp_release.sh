40838ff3bb6401be389723dffc89348ed04bc8c1
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2024-01-22 11:33:14 +0200

    Update zp_release.sh

--- src/ontology/zp_release.sh
+++ src/ontology/zp_release.sh
@@ -2,9 +2,10 @@
 set -e
 IMP=true
 PAT=true
-#ODK_TAG=v1.4.3 ./run.sh make IMP=false PAT=false MIR=false COMP=false zp_pipeline_prepare_data -B
-#ODK_TAG=v1.4.3 ./run.sh make IMP=false PAT=true MIR=false COMP=true templates patterns -B
-#ODK_TAG=v1.4.3 ./run.sh make IMP=$IMP PAT=false MIR=$IMP COMP=false refresh-merged -B
-#ODK_TAG=v1.4.3 ./run.sh make IMP=false PAT=true MIR=false COMP=true patterns -B
+ODK=v1.4.3
+ODK_TAG=$ODK ./run.sh make IMP=false PAT=false MIR=false COMP=false zp_pipeline_prepare_data -B
+ODK_TAG=$ODK ./run.sh make IMP=false PAT=true MIR=false COMP=true templates patterns -B
+ODK_TAG=$ODK ./run.sh make IMP=$IMP PAT=false MIR=$IMP COMP=false refresh-merged -B
+ODK_TAG=$ODK ./run.sh make IMP=false PAT=true MIR=false COMP=true patterns -B
 SHARED="remove -T blacklist_eqs.txt --axioms equivalent --preserve-structure false"
-ODK_TAG=v1.4.3 ./run.sh make SHARED_ROBOT_COMMANDS="$SHARED" prepare_release_fast -B
+ODK_TAG=$ODK ./run.sh make SHARED_ROBOT_COMMANDS="$SHARED" prepare_release_fast -B
