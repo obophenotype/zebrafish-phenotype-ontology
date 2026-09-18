4a3e27c95aeb52d969e99c97f5aa919fca53d0e2
Patrick Golden <ptgolden@email.unc.edu>
2026-09-18 10:40:12 -0400

    Overhaul zp_release.sh script (remove `-B`)
    This commit takes a full run of the ZP release pipeline on my machine
    from 8.5 hours down to 17 minutes.
    
    The overwhelming bottleneck in the old pipeline were `dosdp generate`
    executions. Those executions go from 586 before this commit, to just 10.
    The reason so many invocations were happening was because of the `-B`
    flag: `dosdp generate` was being run over and over, with identical
    outputs, for as many patterns as there were in the patterns directory.
    (This happened multiple times). Each of those runs takes a little less
    than a minute on this machine.
    
    `-B`/`--always-make` flags were removed from all but one invocation in
    the script (`zp_labels.csv` has no dependencies and is checked into
    version control, so it requires forcing). I would have added
    `definitions.owl` as a dependency, but it reintroduces the `dosdp
    generate` explosion. So for now, the `zp_labels.csv` step stays a forced
    remake.
    
    The other change here is to use `PAT=true` for the
    `zp_pipeline_prepare_data` step. The pipeline was not running in its
    current state because it was set to `PAT=false`.
    
    ---
    
    Output was tested against a baseline run at b72b0cd and came out
    identical except for non-deterministic ordering output that still exists
    in some situations.

--- src/ontology/zp_release.sh
+++ src/ontology/zp_release.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 set -e
-IMP=true
-PAT=true
-ODK=v1.5
-ODK_TAG=$ODK ./run.sh make IMP=false PAT=false MIR=false COMP=false zp_pipeline_prepare_data -B
-ODK_TAG=$ODK ./run.sh make IMP=false PAT=true MIR=false COMP=true templates patterns -B
-ODK_TAG=$ODK ./run.sh make IMP=$IMP PAT=false MIR=$IMP COMP=false refresh-merged -B
-ODK_TAG=$ODK ./run.sh make IMP=false PAT=true MIR=false COMP=true patterns -B
+ODK=v1.6.1
+ODK_TAG=$ODK ./run.sh make -B zp_labels.csv
+ODK_TAG=$ODK ./run.sh make IMP=false PAT=true MIR=false COMP=false zp_pipeline_prepare_data
+ODK_TAG=$ODK ./run.sh make IMP=false PAT=true MIR=false COMP=true templates patterns
+ODK_TAG=$ODK ./run.sh make IMP=true PAT=false MIR=true COMP=false refresh-merged
+ODK_TAG=$ODK ./run.sh make IMP=false PAT=true MIR=false COMP=true patterns
 SHARED="remove -T blacklist_eqs.txt --axioms equivalent --preserve-structure false query --update ../sparql/rename-obsolete-classes.ru "
-ODK_TAG=$ODK ./run.sh make SHARED_ROBOT_COMMANDS="$SHARED" prepare_release_fast -B
+ODK_TAG=$ODK ./run.sh make SHARED_ROBOT_COMMANDS="$SHARED" prepare_release_fast
